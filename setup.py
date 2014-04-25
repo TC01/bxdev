0c608081fef2084c93bf7fd9571db9096d0346a2
Ben Rosser <rosser.bjr@gmail.com>
2014-04-25 10:15:39 -0400

    Added long description to setup.py and updated gitignore.

--- setup.py
+++ setup.py
@@ -6,6 +6,39 @@ import sys
 
 from distutils.core import setup
 
+long_desc = """BasicX is a programming language for a series of microcontrollers designed and
+built by NetMedia, Inc. These microcontrollers, the BX01, the BX24, and BX35, 
+are little more than a modified AVR with a bootloader. However, the presence of
+the bootloader means that we can only program them using the BasicX language.
+
+This language is feature-complete with VB 6, and can only be compiled by an IDE 
+nearly ten-years old.
+
+These tools are written to change that.
+
+There are three scripts, bxbuild, bxdebug, and bxproj, and a suite of shared
+libraries they all depend upon. bxproj will create new BasicX projects, bxbuild
+will compile them and download them to target devices, and bxdebug will read
+debugging messages being transmitted over RS232 serial communication.
+
+For more information about BasicX, see http://www.basicx.com/
+BasicX is a programming language for a series of microcontrollers designed and
+built by NetMedia, Inc. These microcontrollers, the BX01, the BX24, and BX35, 
+are little more than a modified AVR with a bootloader. However, the presence of
+the bootloader means that we can only program them using the BasicX language.
+
+This language is feature-complete with VB 6, and can only be compiled by an IDE 
+nearly ten-years old.
+
+These tools are written to change that.
+
+There are three scripts, bxbuild, bxdebug, and bxproj, and a suite of shared
+libraries they all depend upon. bxproj will create new BasicX projects, bxbuild
+will compile them and download them to target devices, and bxdebug will read
+debugging messages being transmitted over RS232 serial communication.
+
+For more information about BasicX, see http://www.basicx.com/"""
+
 scripts = ["bxbuild", "bxdebug", "bxproj"]
 if sys.platform == "win32":
 	newScripts = []
@@ -24,7 +57,7 @@ if sys.platform == "win32":
 setup(  name = "bxdev",
 	version = "0.5",
 	description = "BasicX development libraries",
-	long_description = "Software to debug BasicX projects using serial communications (www.basicx.com)",
+	long_description = long_desc,
 	author = "Ben Rosser",
 	license = "MIT",
 	author_email = "rosser.bjr@gmail.com",
